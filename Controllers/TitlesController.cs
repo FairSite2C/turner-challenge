d7148449be9c7583674bc191390c18b0aca4ae17
Neal Lowrey <lowrey.neal@gmail.com>
2018-07-24 15:04:07 -0400

    turner-challenge have communications and data input and display

--- Controllers/TitlesController.cs
+++ Controllers/TitlesController.cs
@@ -5,20 +5,105 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.KeyVault;
 using System.Configuration;
- 
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+
 namespace turner_challenge.Controllers
 {
+    public class Title{
+        
+    [BsonElement("TitleName")]
+    public string TitleName {get;set;}
+    }
+
     public class TitlesController : Controller
     {
+        string host = "mongodb://readonly:turner@ds043348.mongolab.com:43348/dev-challenge";
+        //string host="mongodb://turner@ds043348.mongolab.com:43348";
+        string dbName = "dev-challenge";
+        
         [HttpGet]
         [Route("api/titles/getTitles")]
-        public List<string> GetTitles()
+        public async Task<List<string>> GetTitles(string searchTerm)
         {
-              Database database = client.CreateDatabaseQuery().Where(db => db.Id == databaseId).AsEnumerable().FirstOrDefault();
+            string collectionName = "Titles";
+
+            var retVal = new List<string>();
+            try
+            {
+                var database = getDatabase();
  
-            return new List<string> { "Gone Wih Wind", "value2" };
+                var titles = database.GetCollection<BsonDocument>(collectionName);
+                var builder = Builders<BsonDocument>.Filter;
+                var filter1 = builder.Regex("TitleName",new BsonRegularExpression(searchTerm, "i"));
+
+                using (var cursor = await titles.FindAsync(filter1))
+                {
+                    while (await cursor.MoveNextAsync())
+                    {
+                        var batch = cursor.Current;
+                        foreach (var document in batch)
+                        {
+                            retVal.Add(document["TitleName"].ToString());        
+                        }
+                    }
+                }
+ 
+       
+            }
+            catch (Exception ex)
+            {
+                string msg = ex.Message;
+            }
+
+            return retVal;
         }
 
+        [HttpGet]
+        [Route("api/titles/getDetail")]
+        public async Task<BsonDocument> GetDetail(string title)
+        {
+            string collectionName = "Titles";
+
+            BsonDocument retVal = null;
+
+            try
+            {
+                var database = getDatabase();
+ 
+                var titles = database.GetCollection<BsonDocument>(collectionName);
+                var builder = Builders<BsonDocument>.Filter;
+                var filter1 = builder.Eq("TitleName",title);
+
+                using (var cursor = await titles.FindAsync(filter1))
+                {
+                    while (await cursor.MoveNextAsync())
+                    {
+                        var batch = cursor.Current;
+                        foreach (var document in batch)
+                        {
+                            retVal = document;        
+                        }
+                    }
+                }
+ 
+       
+            }
+            catch (Exception ex)
+            {
+                string msg = ex.Message;
+            }
+
+            return retVal;
+        }
+
+        private IMongoDatabase getDatabase(){
+            MongoClient client = new MongoClient( new MongoDB.Driver.MongoUrl(host));
+            return client.GetDatabase(dbName);
+        } 
     }
       
 }
--- wwwroot/scripts/turner.js
+++ wwwroot/scripts/turner.js
@@ -1,9 +1,58 @@
-$(function(){
+function titleDetail(valIn){
+    var that = this;
+}
 
-$.get('/api/titles/getTitles')
-    .done(function (data) {
-        for (var i = 0;i<data.length;i++){
-            alert (data[i]);
+function koController(){
+    var that = this;
+    that.searchVal = ko.observable('');
+    that.titles = ko.observableArray([]);
+
+    that.titleDetails = new titleDetail();
+
+    that.searchVal.subscribe(function (newValue) {
+        that.titles.removeAll();
+        if (newValue.length > 2){
+            $.get('/api/titles/getTitles',{searchTerm:newValue})
+            .done(function (data) {
+                that.titles(data);
+            });
         }
     });
+
+    that.selectTitle = function(index){
+        var ix=index();
+        var titleVal = that.titles()[ix];
+        $.get('/api/titles/getDetail',{title:titleVal})
+            .done(function (data) {
+                that.titleDetail = new titleDetail(data);       
+            }); 
+    }
+
+}
+
+var koBoss = null;
+
+$(function(){
+
+    $("#searchText").keyup(function(){
+        var val = $("#searchText").val();    
+        $.get('/api/titles/getTitles',{searchTerm:val})
+            .done(function (data) {
+                for (var i = 0;i<data.length;i++){
+            
+                }
+            });
+    });
+
+    $("#testDetail").click(function(){
+        var val = 'Annie Hall';    
+        $.get('/api/titles/getDetail',{title:val})
+            .done(function (data) {
+
+            });
+    });
+
+    koBoss = new koController();
+    ko.applyBindings(koBoss, $('#titles')[0]);
+
 });
